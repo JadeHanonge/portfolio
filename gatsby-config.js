bb51289504d5dc3aad0b68d3b07cf96a53838745
JadeHanonge <jade.hanonge@gmail.com>
2024-09-19 13:57:22 +0200

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Jade.Hanonge`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
---
+++ src/pages/projects.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Projects = () => {
+    return <h1>Projects page</h1>;
+};
+
+export default Projects;
