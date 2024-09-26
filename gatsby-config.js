7377710532fa937cb6223ee389d2f182c8dc2b65
JadeHanonge <jade.hanonge@gmail.com>
2024-09-26 16:42:32 +0200

    correction pour affichez les img

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,17 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    "gatsby-plugin-image",
+    
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-image",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },  
+    },
+
   ],
 }
