6181afc5c68b311258afc7a627536cd71b400318
JadeHanonge <jade.hanonge@gmail.com>
2024-09-27 15:40:53 +0200

    correction img

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-image",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images/`,
+        path: `${__dirname}/public/images/`,
       },  
     },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-image",
+    
 
   ],
 }
