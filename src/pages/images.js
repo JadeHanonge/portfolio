ef0ab09883167d0f5e6e917dbd458e0a15e8c62d
JadeHanonge <jade.hanonge@gmail.com>
2024-09-27 16:33:02 +0200

    correction img3

---
+++ src/pages/images.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+//import NavBarre from "../components/navBarre";
+import { StaticImage } from "gatsby-plugin-image";
+
+
+const Images = () => {
+    return (
+        <StaticImage src="../images/projet1.jpg"/>
+      );
+};
+
+export default Images;
